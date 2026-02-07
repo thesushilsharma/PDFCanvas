68ebcfbc4f0f538db4b0c32b3751f3d3bf95b59f
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2026-02-08 03:40:57 +0400

    hot fix for pages

--- src/app/(pages)/templates/page.tsx
+++ src/app/(pages)/templates/page.tsx
@@ -1,7 +1,10 @@
-function Page() {
-  return (
-    <div>Templates</div>
-  )
-}
+import { Suspense } from "react";
+import PDFGenerator from "@/components/pdf/PDFGenerator";
 
-export default Page
+export default async function Page() {
+  return (
+    <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading PDF Generator...</div>}>
+      <PDFGenerator />
+    </Suspense>
+  );
+}
